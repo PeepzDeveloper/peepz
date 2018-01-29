5bda6f6d3f067ab8c375da45725b02c9c0f90ee0
Albert Scherman <albert@peepz.co.za>
2018-01-29 20:25:32 +0200

    Added google maps to jobs; Fixed error in formfunctions; Fixed bug in peepzsearch.

--- functions/conset/config.php
+++ functions/conset/config.php
@@ -5,7 +5,7 @@
   defined('DB_USER') ? null : define('DB_USER', 'craigv1_adminpg');
   defined('DB_PASS') ? null : define('DB_PASS', 'P@ssw0rd1');
 
-  defined('SITE_ROOT') ? null : define('SITE_ROOT', $_SERVER["DOCUMENT_ROOT"] . "/peepz/");
+  defined('SITE_ROOT') ? null : define('SITE_ROOT', $_SERVER["DOCUMENT_ROOT"] . "/");
 
   defined('TEMPLATE_DIR') ? null : define('TEMPLATE_DIR', SITE_ROOT . 'Templates/');
 ?>
